470b0bdb2cad6f47f5d4db381df7e8cd9dd1f54d
Jengyoung <young_pallete@naver.com>
2021-06-29 18:26:22 +0900

    nav__navbar 구현

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+window.addEventListener('DOMContentLoaded', () => {
+    function openBar(e: Event): void {
+        const $navBar: HTMLElement = document.querySelector('.nav__navbar');
+        $navBar.classList.toggle('active');
+    }
+    document.querySelector('.nav__btn').addEventListener('click', openBar)
+})
