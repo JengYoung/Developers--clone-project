ede75084cb6cbf891a75d1adafd98ac1ef6beee4
Jengyoung <young_pallete@naver.com>
2021-06-30 11:16:33 +0900

    [ADD] Header 레이아웃 구현

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51350" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55695" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
